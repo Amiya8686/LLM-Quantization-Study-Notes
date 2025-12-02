9082f75c5058618ee7d5d8f981bfe94e3bd834dd
Amiya8686 <1580714256@qq.com>
2025-12-02 12:42:04 +0800

    note the KVcache

--- 大模型量化知识架构/transformer详解/代码实现/transformer.py
+++ 大模型量化知识架构/transformer详解/代码实现/transformer.py
@@ -3,12 +3,15 @@ from torch import nn                                #导入神经网络模块（
 import torch.nn.functional as F                     #神经网络的数学函数
 import math                                         #python自带的数学模块
 
+
 #词嵌入层
 class TokenEmbedding(nn.Embedding):
     def __init__(self,vocab_size,d_model):
         super(TokenEmbedding,self).__init__(vocab_size,d_model,padding_idx=1)
     
 #位置嵌入
+
+
       
 
 
